ec4b38712719932b8654d96236738a0333e8f18b
Themahmud <71950409+Themahmud@users.noreply.github.com>
2022-10-29 00:05:52 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import "./style.css";
 
 export default function App() {
   return (
+    <Router>
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
+    </Router>
+
+   
   );
 }
+
